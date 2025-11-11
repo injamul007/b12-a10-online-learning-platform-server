cb213f74a3d55bdb24505e6756d51099ce86d147
injamul007 <injamul007hoque@gmail.com>
2025-11-12 02:52:56 +0600

    create database and collection in mongodb and in server which is index.js and create get api for all the courses

--- index.js
+++ index.js
@@ -28,6 +28,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db('skilledHubDB');
+    const coursesCollection = db.collection('courses');
+
+    //? get api for get all the courses
+    app.get('/courses', async(req,res) => {
+      const cursor = coursesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
