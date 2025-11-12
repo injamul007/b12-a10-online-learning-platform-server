bb6d7387c3e6fae32e491cffcd77abf6a254c205
injamul007 <injamul007hoque@gmail.com>
2025-11-12 10:13:55 +0600

    create api for get single course data in courses collections

--- index.js
+++ index.js
@@ -39,6 +39,14 @@ async function run() {
       res.send(result);
     })
 
+    //? get api for get single course from courses
+    app.get('/courses/:id', async(req,res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await coursesCollection.findOne(query)
+      res.send(result)
+    })
+
     //? post api for create a post in the courses
     app.post('/courses', async(req,res) => {
       const newCourse = req.body
