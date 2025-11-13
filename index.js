f02dbbd5c3e0b2ca1decfc97ebe6bae23830dde2
injamul007 <injamul007hoque@gmail.com>
2025-11-13 20:44:12 +0600

    get api for my Enrolled courses

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,94 +16,102 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', (req,res) => {
-  res.send('SkilledHub - Online Learning Platform Server is Running...')
+app.get("/", (req, res) => {
+  res.send("SkilledHub - Online Learning Platform Server is Running...");
 });
-
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db = client.db('skilledHubDB');
-    const coursesCollection = db.collection('courses');
-    const enrolledCollection = db.collection('enrolled');
+    const db = client.db("skilledHubDB");
+    const coursesCollection = db.collection("courses");
+    const enrolledCollection = db.collection("enrolled");
 
     //? get api for get all the courses
-    app.get('/courses', async(req,res) => {
+    app.get("/courses", async (req, res) => {
       const email = req.query.email;
-      const query = {}
-      if(email) {
+      const query = {};
+      if (email) {
         query["instructor.email"] = email;
       }
       const cursor = coursesCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
     //? get api for get single course from courses
-    app.get('/courses/:id', async(req,res) => {
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await coursesCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/courses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coursesCollection.findOne(query);
+      res.send(result);
+    });
 
     //? post api for create a post in the courses
-    app.post('/courses', async(req,res) => {
-      const newCourse = req.body
+    app.post("/courses", async (req, res) => {
+      const newCourse = req.body;
       const result = await coursesCollection.insertOne(newCourse);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     //? delete api for delete a data in the courses
-    app.delete('/courses/:id', async(req,res) => {
+    app.delete("/courses/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await coursesCollection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await coursesCollection.deleteOne(query);
+      res.send(result);
+    });
 
     //? patch api for update some data in the courses
-    app.patch('/courses/:id', async(req,res) => {
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const updatedCourse = req.body
+    app.patch("/courses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedCourse = req.body;
       const update = {
-        $set: updatedCourse
-      }
-      const options = {}
-      const result = await coursesCollection.updateOne(query, update, options)
-      res.send(result)
-    })
+        $set: updatedCourse,
+      };
+      const options = {};
+      const result = await coursesCollection.updateOne(query, update, options);
+      res.send(result);
+    });
 
     //? get api from using query email in my added courses
-    app.get('/my-courses', async(req,res) => {
+    app.get("/my-courses", async (req, res) => {
       const email = req.query.email;
-      const query = {}
-      if(email) {
+      const query = {};
+      if (email) {
         query["instructor.email"] = email;
       }
       const cursor = coursesCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
     //? post api for my Enrolled courses in new db collection
-    app.post('/my-enrolled', async(req,res) => {
-      const enrolledCourse = req.body
-      const result = await enrolledCollection.insertOne(enrolledCourse)
-      res.send(result)
-    })
+    app.post("/my-enrolled", async (req, res) => {
+      const enrolledCourse = req.body;
+      const result = await enrolledCollection.insertOne(enrolledCourse);
+      res.send(result);
+    });
 
+    //? get api for my Enrolled courses
+    app.get("/my-enrolled", async (req, res) => {
+      const email = req.query.email;
+      const cursor = enrolledCollection.find({enrolled_by: email});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -112,5 +120,7 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`SkilledHub - Online Learning Platform is Running on Port: ${port}`)
-})
+  console.log(
+    `SkilledHub - Online Learning Platform is Running on Port: ${port}`
+  );
+});
