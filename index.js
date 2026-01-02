6b3cc87f8586a379979cdd77731f3dbce984e13e
injamul007 <injamul007hoque@gmail.com>
2026-01-02 18:33:11 +0600

    update and add the npm run dev command in package.jsons scripts

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const client = new MongoClient(uri, {
     strict: true,
     deprecationErrors: true,
   },
-});
+});  
 
 app.get("/", (req, res) => {
   res.send("SkilledHub - Online Learning Platform Server is Running...");
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const db = client.db("skilledHubDB");
     const coursesCollection = db.collection("courses");
@@ -115,9 +115,9 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
