d6f9b09efd0536609550668f7b86b9d8ca973f41
injamul007 <injamul007hoque@gmail.com>
2026-01-31 00:50:38 +0600

    commented all the mongodb connector before deploy to vercel

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("skilledHubDB");
     const coursesCollection = db.collection("courses");
@@ -192,8 +192,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
